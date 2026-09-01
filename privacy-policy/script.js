6d9ec0b6876c9132c43b7a0deb0bd6a1726e865a
mohammadjamilu <108686841+mohammadjamilu@users.noreply.github.com>
2026-09-02 03:21:29 +0600

    Update landing page: ChatMakky - Privacy Policy (path: /privacy-policy) - 2026-09-01T21:21:26.852Z

--- privacy-policy/script.js
+++ privacy-policy/script.js
@@ -1,3 +1,14 @@
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2501386813634781');
+fbq('track', 'PageView');
+
 (function () {
       const themeToggle = document.querySelector('.theme-toggle');
       const themeIcon = themeToggle ? themeToggle.querySelector('i') : null;
